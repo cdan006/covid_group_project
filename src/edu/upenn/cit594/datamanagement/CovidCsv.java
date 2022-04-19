53cae99aa0defb1326c5af82a13e7b7e98ef37ee
Chris Daniele <cdan006@seas.upenn.edu>
2022-04-19 13:31:25 -0400

    initial set up

--- src/edu/upenn/cit594/datamanagement/CovidCsv.java
+++ src/edu/upenn/cit594/datamanagement/CovidCsv.java
@@ -1,4 +1,24 @@
 package edu.upenn.cit594.datamanagement;
 
-public class CovidCsv {
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class CovidCsv implements CovidReader {
+    protected String filename;
+    public CovidCsv(String name) {
+        filename = name;
+    }
+
+    @Override
+    public ArrayList<Covid> getCovidList() throws IOException {
+
+    }
+
+    public String getFileName() {
+        return filename;
+    }
+
+
+
+
 }
